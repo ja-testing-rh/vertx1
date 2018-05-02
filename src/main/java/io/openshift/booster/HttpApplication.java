8bcfbbbf43d19b3ab5e74786639bd55fffcea91b
Jaseem Abid <jaseemabid@gmail.com>
2018-05-02 17:18:11 +0530

    Another string change to test this out

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Hello there, %s!";
+  protected static final String template = "Namaskara %s!";
 
   @Override
   public void start(Future<Void> future) {
